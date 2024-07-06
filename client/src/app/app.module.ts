b70a158e6f1f720f1a894c6212511eb7daa707d6
kavi castelo <kavindu123450@gmail.com>
2024-07-06 19:41:21 +0530

    Popular Jobs section

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from "@angular/material/icon";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
