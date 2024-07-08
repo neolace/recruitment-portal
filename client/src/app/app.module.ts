3e2ca2715738b1efe108e138db59a5199349ef4c
kavi castelo <kavindu123450@gmail.com>
2024-07-08 18:35:21 +0530

    job list 1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HomeComponent } from './components/home/home.component';
 import {MatIconModule} from "@angular/material/icon";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { JobDetailsComponent } from './components/shared/job-details/job-details.component';
+import { JobComponent } from './components/job/job.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    JobDetailsComponent
+    JobDetailsComponent,
+    JobComponent
   ],
     imports: [
         BrowserModule,
