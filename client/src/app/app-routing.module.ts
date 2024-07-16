bcbfd15112bc3eb56f8b1c2d9329872c012c76ac
kavi castelo <kavindu123450@gmail.com>
2024-07-16 17:51:56 +0530

    apply form
    Apply for job form dummy content

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {JobDetailsComponent} from "./components/shared/job-details/job-details.component";
 import {JobComponent} from "./components/job/job.component";
+import {JobApplyComponent} from "./components/shared/job-apply/job-apply.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'job', component: JobComponent },
   { path: 'job-details', component: JobDetailsComponent },
+  { path: 'job-apply', component: JobApplyComponent },
 ];
 
 @NgModule({
