65a303467b1accd6a048b29f3f274c244bc6c33f
kavi castelo <kavindu123450@gmail.com>
2024-07-23 23:48:05 +0530

    enhanced map

--- client/src/app/components/contact/contact.component.ts
+++ client/src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {GoogleMap, MapInfoWindow, MapMarker} from "@angular/google-maps";
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap | any;
+  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow | any;
+
   zoom = 12;
   center: google.maps.LatLngLiteral | any;
   options: google.maps.MapOptions = {
@@ -16,14 +20,30 @@ export class ContactComponent implements OnInit {
     maxZoom: 15,
     minZoom: 8,
   };
+  markers:any = [
+    {
+      position: {
+        lat: 6.918604,
+        lng: 79.865564,
+      },
+      label: {
+        color: '#5faee3',
+        text: 'We are here',
+      },
+      title: 'SPARKC pvt ltd',
+      info: 'Visit our location',
+      options: {
+        animation: google.maps.Animation.BOUNCE,
+      },
+    }
+  ];
+  infoContent = '';
 
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
-    });
+    this.center = {
+      lat: 6.918604,
+      lng: 79.865564,
+    };
   }
 
   zoomIn() {
@@ -32,6 +52,12 @@ export class ContactComponent implements OnInit {
 
   zoomOut() {
     if (this.zoom > this.options.minZoom!) this.zoom--;
+  }
+
+  openInfo(marker: HTMLElement, content: any) {
+    const mark = marker as unknown as MapMarker;
+    this.infoContent = content;
+    this.info.open(mark);
   }
 
 }
