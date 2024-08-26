867f58c206ddecc2226c3e1c761771863e100e9b
kavi castelo <kavindu123450@gmail.com>
2024-08-26 19:09:27 +0530

    hiring banner

--- client/src/app/components/companies/companies.component.ts
+++ client/src/app/components/companies/companies.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-companies',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class CompaniesComponent {
 
+  constructor(private router: Router) { }
+  moveToRegister() {
+    this.router.navigate(['/register'], {queryParams: {from: 'companies'}});
+  }
 }
