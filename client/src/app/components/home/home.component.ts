e4a61f3f52823a303bc63332c409b73d2af4a8fa
kavi castelo <kavindu123450@gmail.com>
2024-09-07 22:02:00 +0530

    test companies data in home page

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {jobAdDataStrore} from "../../shared/data-store/JobAd-data-strore";
+import {companyDataStore} from "../../shared/data-store/company-data-store";
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,8 @@ export class HomeComponent {
 
   heart: boolean = false; // test
   jobAdDataStrore: any = jobAdDataStrore;
+
+  companyDataStore: any = companyDataStore;
 
   constructor(private router: Router) { }
   moveToRegister() {
