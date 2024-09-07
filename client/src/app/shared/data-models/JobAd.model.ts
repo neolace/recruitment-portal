47138d17127c5b6761a1fa0f4dbe461846365a5c
kavi castelo <kavindu123450@gmail.com>
2024-09-07 18:52:24 +0530

    job details test data store

---
+++ client/src/app/shared/data-models/JobAd.model.ts
@@ -0,0 +1,26 @@
+export interface JobAdModel {
+  id: any;
+  companyId: any;
+  companyName: any;
+  companyLogo: any;
+  companyLevel: any;
+  title: any;
+  description: any;
+  responsibilities: any;
+  requirements: any;
+  experience: any;
+  education: any;
+  skills: any;
+  totalOpenings: any;
+  jobBanner: any;
+  employeeType: any;
+  location: any;
+  jobType: any;
+  exShortDesc: any;
+  eduShortDesc: any;
+  minSalary: any;
+  maxSalary: any;
+  datePosted: any;
+  expiryDate: any;
+  url: any;
+}
