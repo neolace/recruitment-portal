86e16e87eb3de523729522b5c6ba40440ac670eb
kavi castelo <kavindu123450@gmail.com>
2024-09-07 21:51:42 +0530

    companies test data

---
+++ client/src/app/shared/data-models/Company.model.ts
@@ -0,0 +1,31 @@
+export interface CompanyModel {
+  id?: any;
+  name?: any;
+  shortDescription?: any;
+  companyStory?: any;
+  companyLevel?: any;
+  logo?: any;
+  location?: any;
+  profileBanner?: any;
+  image1?: any;
+  image2?: any;
+  image3?: any;
+  foundedDate?: any;
+  founderName?: any;
+  headquarters?: any;
+  numberOfEmployees?: any;
+  website?: any;
+  linkedin?: any;
+  twitter?: any;
+  facebook?: any;
+  instagram?: any;
+  youtube?: any;
+  tiktok?: any;
+  contactEmail?: any;
+  contactNumber?: any;
+  postedJobs?: any;
+  joinedDate?: any;
+  isVerified?: any;
+  followers?: any;
+  following?: any;
+}
