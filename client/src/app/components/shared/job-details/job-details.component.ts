5f79158e563adda2fea32ed916e95a766acce483
kavi castelo <kavindu123450@gmail.com>
2024-09-08 15:48:05 +0530

    job details with dynamic test dataset

--- client/src/app/components/shared/job-details/job-details.component.ts
+++ client/src/app/components/shared/job-details/job-details.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {jobAdDataStrore} from "../../../shared/data-store/JobAd-data-strore";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -6,6 +8,21 @@ import {Component} from '@angular/core';
   templateUrl: './job-details.component.html',
   styleUrls: ['./job-details.component.scss']
 })
-export class JobDetailsComponent {
+export class JobDetailsComponent implements OnInit{
 
+  jobDataStore: any = jobAdDataStrore;
+  filteredJobDataStore: any[] = [];
+  jobPostId: any;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.jobPostId = this.router.url.split('/')[2];
+    this.filterJobData();
+  }
+
+  filterJobData(): any[] {
+    this.filteredJobDataStore = this.jobDataStore.filter((job: any) => job.id === this.jobPostId);
+    return this.filteredJobDataStore;
+  }
 }
