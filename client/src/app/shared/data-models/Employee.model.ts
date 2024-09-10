7d0369a25593c48a459afbf7fcd3265e55e9595d
kavi castelo <kavindu123450@gmail.com>
2024-09-10 23:51:50 +0530

    add missing setting components

---
+++ client/src/app/shared/data-models/Employee.model.ts
@@ -0,0 +1,21 @@
+export interface EmployeeModel {
+  id?: any;
+  firstname?: any;
+  lastname?: any;
+  occupation?: any;
+  image?: any;
+  coverImage?: any;
+  dob?: any;
+  email?: any;
+  resume?: any;
+  intro?: any;
+  skills?: any;
+  experiences?: any;
+  education?: any;
+  contactInfo?: any;
+  socialLinks?: any;
+  accountNotifications?: any;
+  marketingNotifications?: any;
+  profileCompleted?: any;
+  profileStatus?: any;
+}
