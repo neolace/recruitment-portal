ae7260c6dfda9510c6f818fff6eb34303dd095ac
kavi castelo <kavindu123450@gmail.com>
2024-09-13 21:06:45 +0530

    user login

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CredentialService} from "../../services/credential.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,41 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  constructor(private router: Router, private credentialService: CredentialService) { }
+
+
+  loginUser() {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      this.credentialService.fetchCredentialByEmail(formData.email).subscribe((response: any) => {
+        if (!response) {
+          console.log("User doesn't exist or something went wrong");
+          return;
+        }
+        if (formData.password === response.password) {
+          if (response.role === 'candidate') {
+            this.router.navigate(['/']);
+          } else if (response.role === 'employer') {
+            if (response.userLevel === "1") {
+              this.router.navigate(['/dashboard']);
+            }
+            else if (response.userLevel === "2") {
+              this.router.navigate(['/pro']);
+            }
+          }
+        } else {
+          console.log("Password is incorrect");
+        }
+      }, error => {
+        console.log(error);
+      });
+    } else {
+      console.log("Form is invalid");
+    }
+  }
 }
