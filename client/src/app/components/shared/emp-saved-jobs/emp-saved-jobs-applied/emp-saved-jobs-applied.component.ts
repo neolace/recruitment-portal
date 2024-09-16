9af5a28ea05854a8c6ca41426546f261c30e38b0
kavi castelo <kavindu123450@gmail.com>
2024-09-17 03:00:18 +0530

    saved job listing and add fav options to job details page

--- client/src/app/components/shared/emp-saved-jobs/emp-saved-jobs-applied/emp-saved-jobs-applied.component.ts
+++ client/src/app/components/shared/emp-saved-jobs/emp-saved-jobs-applied/emp-saved-jobs-applied.component.ts
@@ -1,16 +1,71 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {EmployeeService} from "../../../../services/employee.service";
+import {AuthService} from "../../../../services/auth.service";
+import {ToastrService} from "ngx-toastr";
+import {jobAdDataStrore} from "../../../../shared/data-store/JobAd-data-strore";
 
 @Component({
   selector: 'app-emp-saved-jobs-applied',
   templateUrl: './emp-saved-jobs-applied.component.html',
   styleUrls: ['./emp-saved-jobs-applied.component.scss']
 })
-export class EmpSavedJobsAppliedComponent implements AfterViewInit{
+export class EmpSavedJobsAppliedComponent implements AfterViewInit, OnInit {
+
+  employee: any;
+  employeeId: any; //66e5a9836f5a4f722e9e97cf || 66e31aa7217eb911ad764373
+  userSavedIds: any[] = [];
+
+  jobAdDataStore: any[] = jobAdDataStrore; //for test
+  constructor(private employeeService: EmployeeService, private cookieService: AuthService, private toastr: ToastrService ) { }
 
   ngAfterViewInit(): void {
     const icons = document.querySelectorAll('.material-icons');
     icons.forEach((icon) => {
       icon.setAttribute('translate', 'no');
+    });
+  }
+
+  ngOnInit(): void {
+    this.employeeId = this.cookieService.userID();
+    this.getEmployee(this.employeeId);
+  }
+
+  getEmployee(id: any) {
+    this.employeeService.fetchFullEmployee(id).subscribe(
+      (data) => {
+        this.employee = data;
+        this.userSavedIds = this.employee.employee.savedJobs.map((job: any) => job.jobId);
+      },
+      (error: any) => {
+        this.warningMessage('Please Login First to Apply Jobs', 'Reminder');
+      }
+    );
+  }
+
+  filterJobs():any[] {
+    return this.jobAdDataStore.filter((job: any) => this.userSavedIds.includes(job.JobId) && job.status === 'applied' || job.status === 'expired');
+  }
+
+  successMessage(msg: string, title: string) {
+    this.toastr.success(msg, title, {
+      progressBar: true,
+      progressAnimation: 'increasing',
+      closeButton: true,
+    });
+  }
+
+  errorMessage(msg: string, title: string) {
+    this.toastr.error(msg, title, {
+      progressBar: true,
+      progressAnimation: 'decreasing',
+      closeButton: true,
+    });
+  }
+
+  warningMessage(msg: string, title: string) {
+    this.toastr.warning(msg, title, {
+      progressBar: true,
+      progressAnimation: 'decreasing',
     });
   }
 }
--- client/src/app/components/shared/job-details/job-details.component.ts
+++ client/src/app/components/shared/job-details/job-details.component.ts
@@ -1,6 +1,10 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {jobAdDataStrore} from "../../../shared/data-store/JobAd-data-strore";
 import {Router} from "@angular/router";
+import {ValueIncrementService} from "../../../services/value-increment.service";
+import {EmployeeService} from "../../../services/employee.service";
+import {AuthService} from "../../../services/auth.service";
+import {ToastrService} from "ngx-toastr";
 
 
 @Component({
@@ -14,11 +18,21 @@ export class JobDetailsComponent implements OnInit, AfterViewInit{
   filteredJobDataStore: any[] = [];
   jobPostId: any;
 
-  constructor(private router: Router) {}
+  employee: any;
+  employeeId: any; //66e5a9836f5a4f722e9e97cf || 66e31aa7217eb911ad764373
+  userSavedIds: any[] = [];
+
+  constructor(private router: Router,
+              private valueIncrementService: ValueIncrementService,
+              private employeeService: EmployeeService,
+              private cookieService: AuthService,
+              private toastr: ToastrService ) { }
 
   ngOnInit(): void {
     this.jobPostId = this.router.url.split('/')[2];
+    this.employeeId = this.cookieService.userID();
     this.filterJobData();
+    this.getEmployee(this.employeeId);
   }
 
   ngAfterViewInit() {
@@ -28,8 +42,64 @@ export class JobDetailsComponent implements OnInit, AfterViewInit{
     });
   }
 
+  getEmployee(id: any) {
+    this.employeeService.fetchFullEmployee(id).subscribe(
+      (data) => {
+        this.employee = data;
+        this.userSavedIds = this.employee.employee.savedJobs.map((job: any) => job.jobId);
+      },
+      (error: any) => {
+        this.warningMessage('Please Login First to Apply Jobs', 'Reminder');
+      }
+    );
+  }
+
   filterJobData(): any[] {
     this.filteredJobDataStore = this.jobDataStore.filter((job: any) => job.id === this.jobPostId);
     return this.filteredJobDataStore;
+  }
+
+  saveFav(id: string) {
+    this.employeeService.saveFavJobs(this.employeeId, {
+      jobId: id,
+      status: 'saved'
+    }).subscribe((data) => {
+      this.getEmployee(this.employeeId);
+      this.successMessage('Job Saved Successfully', 'Success');
+    }, (error: any) => {
+      this.errorMessage('Something went wrong. Please try again', 'Error');
+    });
+  }
+
+  removeFav(id: string) {
+    this.employeeService.removeFavJobs(this.employeeId, id).subscribe((data) => {
+      this.getEmployee(this.employeeId);
+      this.successMessage('Job Removed Successfully', 'Success');
+    }, (error: any) => {
+      this.errorMessage('Something went wrong. Please try again', 'Error');
+    });
+  }
+
+  successMessage(msg: string, title: string) {
+    this.toastr.success(msg, title, {
+      progressBar: true,
+      progressAnimation: 'increasing',
+      closeButton: true,
+    });
+  }
+
+  errorMessage(msg: string, title: string) {
+    this.toastr.error(msg, title, {
+      progressBar: true,
+      progressAnimation: 'decreasing',
+      closeButton: true,
+    });
+  }
+
+  warningMessage(msg: string, title: string) {
+    this.toastr.warning(msg, title, {
+      progressBar: true,
+      progressAnimation: 'decreasing',
+    });
   }
 }
