68fd098dd10b38d0babd0ce6db56af73894caab3
kavi castelo <kavindu123450@gmail.com>
2024-09-18 22:40:24 +0530

    batch APIs for company details

--- src/main/java/com/hris/HRIS_job_portal/Service/CmpPostedJobsService.java
+++ src/main/java/com/hris/HRIS_job_portal/Service/CmpPostedJobsService.java
@@ -3,9 +3,11 @@ package com.hris.HRIS_job_portal.Service;
 import com.hris.HRIS_job_portal.Model.CmpPostedJobsModel;
 import com.hris.HRIS_job_portal.Repository.CmpPostedJobsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class CmpPostedJobsService {
@@ -18,5 +20,13 @@ public class CmpPostedJobsService {
 
     public CmpPostedJobsModel addCmpPostedJobs(CmpPostedJobsModel cmpPostedJobs) {
         return cmpPostedJobsRepository.save(cmpPostedJobs);
+    }
+
+
+    @Async
+    public CompletableFuture<List<CmpPostedJobsModel>> getCmpPostedJobsByCompanyIdAsync(String companyId) {
+        List<CmpPostedJobsModel> cmpPostedJobsList = cmpPostedJobsRepository.findByCompanyId(companyId);
+
+        return CompletableFuture.completedFuture(cmpPostedJobsList);
     }
 }
