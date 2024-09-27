e54222994f9e567b5f00b8d129533c8aad558317
kavi castelo <kavindu123450@gmail.com>
2024-09-28 00:53:13 +0530

    user fetch free dashboard wrapper

--- client/src/app/components/dashboards/free/free-dashboard/free-dashboard.component.ts
+++ client/src/app/components/dashboards/free/free-dashboard/free-dashboard.component.ts
@@ -1,21 +1,48 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../../services/auth.service";
+import {EmployeeService} from "../../../../services/employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-free-dashboard',
   templateUrl: './free-dashboard.component.html',
   styleUrls: ['./free-dashboard.component.scss']
 })
-export class FreeDashboardComponent implements AfterViewInit{
+export class FreeDashboardComponent implements AfterViewInit, OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute, private cookieService: AuthService) { }
+  employeeId: any;
+  employeeName: any;
+  employeeEmail: any;
+  employeeProfile: any;
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private employeeService: EmployeeService,
+              private cookieService: AuthService) { }
+
+  ngOnInit() {
+    this.employeeId = this.cookieService.userID();
+    this.getEmployee(this.employeeId);
+  }
 
   ngAfterViewInit() {
     const icons = document.querySelectorAll('.material-icons');
     icons.forEach((icon) => {
       icon.setAttribute('translate', 'no');
     });
+  }
+
+  getEmployee(id: any) {
+    this.employeeService.fetchFullEmployee(id).subscribe(
+      (data) => {
+        this.employeeEmail = data?.employee?.email;
+        this.employeeName = data?.employee?.firstname + ' ' + data?.employee?.lastname;
+        this.employeeProfile = data?.employee?.image;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
   }
 
   logout() {
