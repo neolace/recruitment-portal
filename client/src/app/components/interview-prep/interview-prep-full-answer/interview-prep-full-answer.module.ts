653df6632cc386ffc21f29fd7cc5760897615c65
kavi castelo <kavindu123450@gmail.com>
2024-10-06 18:35:25 +0530

    interview prep answer UI

--- client/src/app/components/interview-prep/interview-prep-full-answer/interview-prep-full-answer.module.ts
+++ client/src/app/components/interview-prep/interview-prep-full-answer/interview-prep-full-answer.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { InterviewPrepFullAnswerRoutingModule } from './interview-prep-full-answer-routing.module';
 import { InterviewPrepFullAnswerComponent } from './interview-prep-full-answer.component';
+import {NotFoundModule} from "../../shared/not-found/not-found.module";
 
 
 @NgModule({
   declarations: [
     InterviewPrepFullAnswerComponent
   ],
-  imports: [
-    CommonModule,
-    InterviewPrepFullAnswerRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        InterviewPrepFullAnswerRoutingModule,
+        NotFoundModule
+    ]
 })
 export class InterviewPrepFullAnswerModule { }
