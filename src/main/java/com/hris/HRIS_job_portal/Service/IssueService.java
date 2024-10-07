1ccb0dc6c31061f36ee9325104a942e7ef5283cc
kavi castelo <kavindu123450@gmail.com>
2024-10-07 23:02:19 +0530

    report issue

--- src/main/java/com/hris/HRIS_job_portal/Service/IssueService.java
+++ src/main/java/com/hris/HRIS_job_portal/Service/IssueService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class IssueService {
@@ -19,5 +20,16 @@ public class IssueService {
 
     public List<IssueModel> getAllIssues() {
         return issueRepository.findAll();
+    }
+
+    public IssueModel updateAttachment(IssueModel issue) {
+        Optional<IssueModel> issueModel = issueRepository.findById(issue.getId());
+        if (issueModel.isPresent()) {
+            IssueModel existingIssue = issueModel.get();
+            existingIssue.setAttachment(issue.getAttachment());
+
+            return issueRepository.save(existingIssue);
+        }
+        return null;
     }
 }
