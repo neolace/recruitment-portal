789dbc6a40faae2ed0e7169fc431a1c6901e7d52
kavi castelo <kavindu123450@gmail.com>
2024-10-11 01:14:30 +0530

    job post card template 2

--- client/src/app/shared/modules/shared-cards-component.module.ts
+++ client/src/app/shared/modules/shared-cards-component.module.ts
@@ -3,10 +3,12 @@ import {JobPostCard1Component} from "../../components/shared/cards/job-post-card
 import {NgClass, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {SharedPipesModule} from "./shared-pipes.module";
+import {JobPostCard2Component} from "../../components/shared/cards/job-post-card2/job-post-card2.component";
 
 @NgModule({
   declarations: [
-    JobPostCard1Component
+    JobPostCard1Component,
+    JobPostCard2Component
   ],
   imports: [
     NgIf,
@@ -15,7 +17,8 @@ import {SharedPipesModule} from "./shared-pipes.module";
     NgClass
   ],
   exports: [
-    JobPostCard1Component
+    JobPostCard1Component,
+    JobPostCard2Component
   ]
 })
 export class SharedCardsComponentModule {}
