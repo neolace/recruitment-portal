e144da750a3ce2b432b854c9a20ca596b4ba8f09
kavi castelo <kavindu123450@gmail.com>
2024-11-02 21:36:08 +0530

    24/7 page

--- client/src/app/components/shared/content/support/support.component.ts
+++ client/src/app/components/shared/content/support/support.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./support.component.scss']
 })
 export class SupportComponent {
-
+  card1: any = 'Reach out via live chat, email, or phone anytime. No waiting for business hours.';
+  card2: any = 'We don\'t just react—we monitor systems to identify potential issues before they affect you.';
+  card3: any = 'With support centers worldwide, we ensure there\'s always someone available to assist, no matter your location.';
 }
--- client/src/app/components/shared/content/support/support.module.ts
+++ client/src/app/components/shared/content/support/support.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { SupportRoutingModule } from './support-routing.module';
 import { SupportComponent } from './support.component';
+import {SharedPipesModule} from "../../../../shared/modules/shared-pipes.module";
 
 
 @NgModule({
   declarations: [
     SupportComponent
   ],
-  imports: [
-    CommonModule,
-    SupportRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        SupportRoutingModule,
+        SharedPipesModule
+    ]
 })
 export class SupportModule { }
