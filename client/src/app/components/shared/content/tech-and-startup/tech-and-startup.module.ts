b227e824cfd5d375de64c53db82d9786db49eeda
kavi castelo <kavindu123450@gmail.com>
2024-11-03 17:47:42 +0530

    tech and startup jobs page content

--- client/src/app/components/shared/content/tech-and-startup/tech-and-startup.module.ts
+++ client/src/app/components/shared/content/tech-and-startup/tech-and-startup.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { TechAndStartupRoutingModule } from './tech-and-startup-routing.module';
 import { TechAndStartupComponent } from './tech-and-startup.component';
+import {SharedPipesModule} from "../../../../shared/modules/shared-pipes.module";
 
 
 @NgModule({
   declarations: [
     TechAndStartupComponent
   ],
-  imports: [
-    CommonModule,
-    TechAndStartupRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        TechAndStartupRoutingModule,
+        SharedPipesModule
+    ]
 })
 export class TechAndStartupModule { }
