c74798f65efd21c0a0978b516a979a5046d011c8
kavi castelo <kavindu123450@gmail.com>
2024-11-22 04:14:43 +0530

    custom micrometer config attempt 1

--- src/main/java/com/hris/HRIS_job_portal/Config/AsyncConfig.java
+++ src/main/java/com/hris/HRIS_job_portal/Config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package com.hris.HRIS_job_portal.Config;
 
+import com.hris.HRIS_job_portal.Shared.ContextPropagatingTaskDecorator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -14,10 +15,11 @@ public class AsyncConfig {
     @Bean(name = "taskExecutor")
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(5);
-        executor.setQueueCapacity(100);
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(50);
         executor.setThreadNamePrefix("AsyncTask-");
+        executor.setTaskDecorator(new ContextPropagatingTaskDecorator());
         executor.initialize();
         return executor;
     }
