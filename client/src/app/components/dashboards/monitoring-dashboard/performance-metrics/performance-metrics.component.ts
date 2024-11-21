42a013beb922dd0ed5785d4736431f4d1b6c553f
kavi castelo <kavindu123450@gmail.com>
2024-11-22 01:17:04 +0530

    server performance

--- client/src/app/components/dashboards/monitoring-dashboard/performance-metrics/performance-metrics.component.ts
+++ client/src/app/components/dashboards/monitoring-dashboard/performance-metrics/performance-metrics.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MonitoringService} from "../../../../services/monitoring.service";
+import {ThemeService} from "../../../../services/theme.service";
+import {ChartOptions} from "chart.js";
 
 @Component({
   selector: 'app-performance-metrics',
@@ -13,7 +15,7 @@ export class PerformanceMetricsComponent implements OnInit {
   labels: string[] = []; // To store labels for the chart
 
   // Chart Options
-  public lineChartOptions = {
+  public lineChartOptions: ChartOptions<'bar'> = {
     responsive: true,
     plugins: {
       legend: {
@@ -22,27 +24,73 @@ export class PerformanceMetricsComponent implements OnInit {
     },
   };
 
-  constructor(private monitoringService: MonitoringService) {}
+  constructor(private monitoringService: MonitoringService, private themeService: ThemeService) {}
 
   ngOnInit(): void {
     // Fetch all metric names
     this.monitoringService.getPerformanceMetrics().subscribe((response: any) => {
       this.metrics = response.names;
     });
+
+    this.applyTheme()
   }
 
   onMetricSelect(): void {
     if (this.selectedMetric) {
       // Fetch data for the selected metric
       this.monitoringService.getMetricData(this.selectedMetric).subscribe((response: any) => {
-        this.labels = response.measurements.map((_: any, index: any) => `Label ${index + 1}`);
+        this.labels = response.measurements.map((_: any, index: any) => `${_.statistic ? _.statistic : 'Label ' + index + 1}`);
         this.chartData = [
           {
             data: response.measurements.map((measurement: any) => measurement.value),
-            label: this.selectedMetric,
+            label: response.description ? response.description : response.name ? response.name : 'Metric',
           },
         ];
       });
+
+      this.applyTheme()
     }
+  }
+
+  applyTheme(): void {
+    const axisColor = this.themeService.isDarkMode() ? '#fff' : '#222';
+    const gridColor = this.themeService.isDarkMode() ? '#444444' : '#e0e0e0';
+    const tooltipBackgroundColor = this.themeService.isDarkMode() ? '#333333' : '#ffffff';
+    const tooltipFontColor = this.themeService.isDarkMode() ? '#fff' : '#222';
+
+    this.lineChartOptions = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+          labels: {
+            color: axisColor
+          }
+        },
+        tooltip: {
+          backgroundColor: tooltipBackgroundColor,
+          titleColor: tooltipFontColor,
+          bodyColor: tooltipFontColor,
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: axisColor
+          },
+          grid: {
+            color: gridColor
+          }
+        },
+        y: {
+          ticks: {
+            color: axisColor
+          },
+          grid: {
+            color: gridColor
+          }
+        }
+      }
+    };
   }
 }
