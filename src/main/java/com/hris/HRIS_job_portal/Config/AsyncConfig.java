063d9ca1f73ce85fc086aa58399c121c5240f56b
kavi castelo <kavindu123450@gmail.com>
2024-11-22 18:29:26 +0530

    Update AsyncConfig.java

--- src/main/java/com/hris/HRIS_job_portal/Config/AsyncConfig.java
+++ src/main/java/com/hris/HRIS_job_portal/Config/AsyncConfig.java
@@ -1,6 +1,9 @@
 package com.hris.HRIS_job_portal.Config;
 
 import com.hris.HRIS_job_portal.Shared.ContextPropagatingTaskDecorator;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -13,7 +16,7 @@ import java.util.concurrent.Executor;
 public class AsyncConfig {
 
     @Bean(name = "taskExecutor")
-    public Executor taskExecutor() {
+    public Executor taskExecutor(MeterRegistry meterRegistry) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(10);
         executor.setMaxPoolSize(20);
@@ -21,6 +24,14 @@ public class AsyncConfig {
         executor.setThreadNamePrefix("AsyncTask-");
         executor.setTaskDecorator(new ContextPropagatingTaskDecorator());
         executor.initialize();
+
+        ExecutorServiceMetrics.monitor(
+                meterRegistry,
+                executor.getThreadPoolExecutor(),
+                "taskExecutor",
+                Tags.of("executor.name", "AsyncTaskExecutor")
+        );
+
         return executor;
     }
 }
