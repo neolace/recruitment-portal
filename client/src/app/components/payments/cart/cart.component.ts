fa421f58d1dc41dee591fd09fdde7c059f2f09e8
kavi castelo <kavindu123450@gmail.com>
2024-12-11 05:49:16 +0530

    add cart to payment options

--- client/src/app/components/payments/cart/cart.component.ts
+++ client/src/app/components/payments/cart/cart.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class CartComponent {
 
+  card:boolean = false;
+  bank:boolean = false;
+  paypal:boolean = false;
+
+  errorMsg = '';
+
+  selectOption(option: string) {
+    switch (option){
+      case 'card': this.card = true; this.bank = false; this.paypal = false; break;
+      case 'bank': this.bank = true; this.card = false; this.paypal = false; break;
+      case 'paypal': this.paypal = true; this.card = false; this.bank = false;break;
+      default: this.card = false; this.bank = false; this.paypal = false; break;
+    }
+  }
+
+  checkout(){
+    this.errorMsg = '';
+    if (!this.bank && !this.card && !this.paypal){
+      this.errorMsg = 'Please Choose a Valid Payment Option to Checkout';
+      return
+    }
+    if (this.bank && !this.card && !this.paypal){
+      console.log('redirect to bank payout')
+    }
+    if (this.card && !this.bank && !this.paypal){
+      console.log('redirect to card payout')
+    }
+    if (this.paypal && !this.card && !this.bank){
+      console.log('redirect to paypal payout')
+    }
+  }
 }
