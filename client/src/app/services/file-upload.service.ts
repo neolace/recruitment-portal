18cfaa0ea318ca7d9887e30cea028f90c1130250
kavi castelo <kavindu123450@gmail.com>
2024-12-11 23:42:37 +0530

    fix file uploading

--- client/src/app/services/file-upload.service.ts
+++ client/src/app/services/file-upload.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import {finalize, switchMap} from 'rxjs/operators';
-import {Observable} from "rxjs";
+import {from, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +12,8 @@ export class FileUploadService {
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, file);
 
-    return task.snapshotChanges().pipe(
-      finalize(() => console.log('Upload complete')),
-      switchMap(() => fileRef.getDownloadURL())
+    return from(
+        task.snapshotChanges().toPromise().then(() => fileRef.getDownloadURL().toPromise())
     );
   }
 }
