e42792474e8684a03d48e4da0fd28df6c489e758
kavi castelo <kavindu123450@gmail.com>
2025-01-07 00:49:16 +0530

    change guards

--- client/src/app/guards/token-guard.guard.ts
+++ client/src/app/guards/token-guard.guard.ts
@@ -1,40 +1,31 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateFn} from '@angular/router';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 import {environment} from "../../environments/environment";
 import {ApiResponse} from "../DTO/ApiResponse";
 
 @Injectable({
   providedIn: 'root'
 })
-export class TokenGuard  {
+export class TokenGuard {
 
-  baseUrl = environment.apiUrl
-
-  constructor(private http: HttpClient, private router: Router) {}
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  canActivate: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
     const token = route.queryParamMap.get('token');
-
     if (!token) {
       this.router.navigate(['/403']);
       return of(false);
     }
+    const headers = new HttpHeaders({Authorization: `Basic ${btoa('admin:password')}`});
+    return this.http.get<ApiResponse>(`${this.baseUrl}/validate-token?token=${token}`, {headers}).pipe(map(response => response.message === "Valid token"), catchError(error => {
+      this.router.navigate(['/403']);
+      return of(false);
+    }));
+  };
 
-    const headers = new HttpHeaders({
-      Authorization: `Basic ${btoa('admin:password')}`
-    });
-
-    return this.http.get<ApiResponse>(`${this.baseUrl}/validate-token?token=${token}`, { headers }).pipe(
-      map(response => {
-        return response.message === "Valid token";
-      }),
-      catchError(error => {
-        this.router.navigate(['/403']);
-        return of(false);
-      })
-    );
+  constructor(private http: HttpClient, private router: Router) {
   }
+
+  private baseUrl = environment.apiUrl;
 }
