4ae523f6304f2169e5f385305e9aaa68f8b92aa6
kavi castelo <kavindu123450@gmail.com>
2025-01-08 19:47:22 +0530

    safe static page loading

--- src/main/java/com/hris/HRIS_job_portal/Controller/common/AngularController.java
+++ src/main/java/com/hris/HRIS_job_portal/Controller/common/AngularController.java
@@ -10,4 +10,62 @@ public class AngularController {
     public String redirect(@PathVariable String path) {
         return "forward:/index.html";
     }
+
+//    @GetMapping("/{path:^(home|about|job|companies|privacy-policy|terms-and-conditions|pricing|faq|for-companies|employees)$}")
+//    public String serveStatic(@PathVariable String path) {
+//        if (!path.equals("")){
+//            return "forward:/" + path + ".html";
+//        }
+//        return "forward:/index.html";
+//    }
+
+    @GetMapping("/home")
+    public String serveHome() {
+        return "forward:/home.html";
+    }
+
+    @GetMapping("/about")
+    public String serveAbout() {
+        return "forward:/about.html";
+    }
+
+    @GetMapping("/job")
+    public String serveJob() {
+        return "forward:/job.html";
+    }
+
+    @GetMapping("/companies")
+    public String serveCompanies() {
+        return "forward:/companies.html";
+    }
+
+    @GetMapping("/privacy-policy")
+    public String servePrivacyPolicy() {
+        return "forward:/privacy-policy.html";
+    }
+
+    @GetMapping("/terms-and-conditions")
+    public String serveTermsAndConditions() {
+        return "forward:/terms-and-conditions.html";
+    }
+
+    @GetMapping("/pricing")
+    public String servePricing() {
+        return "forward:/pricing.html";
+    }
+
+    @GetMapping("/faq")
+    public String serveFaq() {
+        return "forward:/faq.html";
+    }
+
+    @GetMapping("/for-companies")
+    public String serveForCompanies() {
+        return "forward:/for-companies.html";
+    }
+
+    @GetMapping("/employees")
+    public String serveEmployees() {
+        return "forward:/employees.html";
+    }
 }
