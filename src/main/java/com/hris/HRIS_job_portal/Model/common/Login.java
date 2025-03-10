d0c3593de302772704942ebf380a535b9b4108d8
kavi castelo <kavindu123450@gmail.com>
2025-03-10 19:46:26 +0530

    add meta data for user activity(visit)

--- src/main/java/com/hris/HRIS_job_portal/Model/common/Login.java
+++ src/main/java/com/hris/HRIS_job_portal/Model/common/Login.java
@@ -1,5 +1,6 @@
 package com.hris.HRIS_job_portal.Model.common;
 
+import com.hris.HRIS_job_portal.DTO.common.LoginMetaDTO;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
@@ -16,4 +17,5 @@ public class Login {
     private String id;
     private String userId;
     private List<String> loginDates;
+    private List<LoginMetaDTO> metaData;
 }
